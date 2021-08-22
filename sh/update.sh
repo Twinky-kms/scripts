4de176d0cf574fd25075efe6e27c7ebf3b459d8d
KMSoftware <Twinkykms@gmail.com>
2021-08-22 16:17:21 -0400

    Update update.sh

--- sh/update.sh
+++ sh/update.sh
@@ -6,18 +6,17 @@ genix-cli stop
 genix-cli -testnet stop
 
 rm linux-binaries.zip
-rm -r linux-binaries/
 
 wget https://github.com/Twinky-kms/genix/releases/download/v2.2.3-dev1/linux-binaries.zip 
 unzip linux-binaries.zip 
 
 sleep 10
 
-cd linux-binaries/
 chmod +x genixd genix-cli genix-tx
 mv genixd /usr/bin
 mv genix-cli /usr/bin
 mv genix-tx /usr/bin
+rm genix-qt
 
 genixd 
 genixd -testnet
