253790eb11b0da282a45d8e22a1dc0ae05bdd534
KMSoftware <Twinkykms@gmail.com>
2021-08-23 10:03:35 -0400

    Update update.sh

--- sh/update.sh
+++ sh/update.sh
@@ -2,10 +2,11 @@
 cd /root/
 
 #stop genixd
-genix-cli stop 
+#genix-cli stop 
 genix-cli -testnet stop
 
 rm linux-binaries.zip
+rm genix-qt
 
 wget https://github.com/Twinky-kms/genix/releases/download/v2.2.3-dev1/linux-binaries.zip 
 unzip linux-binaries.zip 
@@ -18,13 +19,13 @@ mv genix-cli /usr/bin
 mv genix-tx /usr/bin
 rm genix-qt
 
-genixd 
+#genixd 
 genixd -testnet
 
 
 sleep 30
 
-genix-cli getinfo
+#genix-cli getinfo
 genix-cli -testnet getinfo
 
 
