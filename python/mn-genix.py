4355a6f4576e347059daa9c8a5f08a9c7cde3379
KMSoftware <Twinkykms@gmail.com>
2021-09-24 20:11:10 -0400

    Update mn-genix.py

--- python/mn-genix.py
+++ python/mn-genix.py
@@ -8,16 +8,11 @@ import csv
 rpc_user = 'rpcuser' #rpc user you set in your genix.conf 
 rpc_password = 'rpcpassword' #rpc password you set in your genix.conf 
 rpc_port = 4444  #rpc port you set in your genix.conf 
-batch_label = 'batch' # example output: batch-1-collat-n (n = collat id#)
-batch_number = 1; # which batch of masternodes 
+batch_label = 'b2' # example output: batch-1-collat-n (n = collat id#)
 ### Dont edit below ###
 
-
-csvArray = [];
-amountToCreate = int(input("How many mns to create?: "))
+fileName = batch_label+'-'+'.csv'
 rpc_url = 'http://127.0.0.1:'+str(rpc_port)
-
-fileName = batch_label+'-'+str(batch_number)+'.csv'
 
 def export_csv():
     with open(fileName, mode='w') as mn_file:
@@ -37,6 +32,26 @@ def instruct_wallet(method, params):
     except:
         print('No response from Wallet, check Bitcoin is running on this machine')
 
+def export_csv():
+    with open(fileName, mode='w') as mn_file:
+        listToStr = ' '.join(map(str, csvArray))
+        mn_file.write(listToStr)
+
+def instruct_wallet(method, params):
+    url = rpc_url
+    payload = json.dumps({"method": method, "params": params})
+    headers = {'content-type': "application/json", 'cache-control': "no-cache"}
+    try:
+        response = requests.request(
+            "POST", url, data=payload, headers=headers, auth=(rpc_user, rpc_password))
+        return json.loads(response.text)
+    except requests.exceptions.RequestException as e:
+        print(e)
+    except:
+        print('No response from Wallet, check Bitcoin is running on this machine')
+
+csvArray = [];
+amountToCreate = int(input("How many mns to create?: "))
 
 i = 1 #start at 1 index. (starting at 0 would make n+1 collats.)
 while i < amountToCreate+1:
@@ -45,9 +60,9 @@ while i < amountToCreate+1:
     txid = str()
     vout = int()
     txIndex = int()
+    csvStr = str()
     com = ","
-    csv = str()
-    
+
     # Get new address
     cAddress = instruct_wallet('getnewaddress', [batch_label+"-collat-"+str(i)])
     if cAddress['error'] != None:
@@ -61,15 +76,15 @@ while i < amountToCreate+1:
         print(oAddress['error'])
     else:
         ownerAddress += oAddress['result']
-        csv += oAddress['result']+com
+        csvStr += oAddress['result']+com
 
     # Send collat
-    collat = instruct_wallet('sendtoaddress', [collatAddress, "100000"])
+    collat = instruct_wallet('sendtoaddress', [collatAddress, "10"])
     if collat['error'] != None:
         print(collat['error'])
     else:
         txid += collat['result']
-        csv += str(collat['result']+com)
+        csvStr += str(collat['result']+com)
 
     # get collat vout
     index = instruct_wallet('gettransaction', [txid])
@@ -77,9 +92,9 @@ while i < amountToCreate+1:
         print(index['error'])
     else:
         vout = int(index['result']['details'][1]['vout'])
-        csv += str(index['result']['details'][1]['vout'])
-        csv += "\n"
-    csvArray.append(csv)
+        csvStr += str(index['result']['details'][1]['vout'])
+        csvStr += "\n"
+    csvArray.append(csvStr)
     # lock unspent
     lock = instruct_wallet('lockunspent', [bool(1 > 10), [
                            {"txid": txid, "vout": vout}]])
